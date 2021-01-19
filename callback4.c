9dc420cfeade01f24ff101ad960fbcfa0f07c9ae
shaunak <shaunak@debian-shaunak.shaunak>
2021-01-19 22:18:11 +0530

    second commit callback4

--- callback4.c
+++ callback4.c
@@ -9,11 +9,16 @@ struct Laptop{
     int price;
     char brand[20];
     void (*setPrice)(struct Laptop *lt, int price);
+    void (*setBrand)(struct Laptop *lt, char *brand);
 };
 
 void set_Price(struct Laptop *lt,int price)
 {
     lt->price = price;
+}
+void set_Brand(struct Laptop *lt, char *brand){
+
+    sprintf(lt->brand,"%s",brand);
 }
 
 
@@ -21,7 +26,10 @@ int main()
 {
     struct Laptop L1;
     L1.setPrice = &set_Price;
+    L1.setBrand = &set_Brand;
     L1.setPrice(&L1,25000);
+    L1.setBrand(&L1,"Lenovo");
+    printf("Brand: %s\n",L1.brand);
     printf("Price: %d\n",L1.price);
     return 0;
 }
