a6e10fcd4c17ebc384fa9813763cae89ad2d6cfc
maitrashaunak <shaunak.sweipl@gmail.com>
2021-01-29 10:24:51 +0530

    Second commit

--- file_io_toFile.c
+++ file_io_toFile.c
@@ -1,15 +1,20 @@
 #include <stdio.h>
 
+/*must terminate the program by Ctrl+D in linux
+to provide EOF, otherwise contents written to 
+the file will not save.
+*/
 
-int main()
+int main(void)
 {
     int c;
     FILE *fp;
+    fp = fopen("NewFile.txt","a");
     while((c=fgetc(stdin))!=EOF){
-     fp = fopen("NewFile.txt","a");
-     fputc(c,fp);
-     fclose(fp);   
+        fputc(c,fp);
     }
+    fclose(fp);
 
- return 0;
+
+    return 0;
 }
