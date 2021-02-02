0c9c574e39723c03bda8091eb4e8e9f7c4b714c4
maitrashaunak <shaunak.sweipl@gmail.com>
2021-02-02 10:18:51 +0530

    second commit strcat returning char *

--- strcat_prog9.c
+++ strcat_prog9.c
@@ -2,22 +2,21 @@
 
 /* concatenate t at the end of s */
 
-void myStrcat(char s[], char t[])
+char *myStrcat(char s[], char t[])
 {
     int i=0;
     int j = 0;
     while(s[i] != '\0') /*not to include \0 so s[i++] not used */
             i++;        /* go to end of s */
     while((s[i++] = t[j++])!= '\0'); /* here to include \0 */
-        
+    return s;    
 }
 
 int main(void)
 {
-    char s[100] = "Hello";
+    char s[100] = "Hello"; /*s must have adequate space */
     char t[] = " World\n";
-    myStrcat(s,t);
-    printf("%s",s);
+    printf("%s",myStrcat(s,t));
     return 0; 
 
 }
